f85b929be5c6596f9ab42188965e1b86632422c7
R.venkatesan <42404698+venkatesanrobot@users.noreply.github.com>
2020-04-01 11:00:27 +0530

    Add files via upload

---
+++ Time-ideal/ideal_time.py
@@ -0,0 +1,41 @@
+import datetime as t
+import winsound
+
+
+time=t.datetime.now()
+time_m=time.minute
+time_h=time.hour
+time_start=time_h*60+time_m
+#print(time_start)
+
+a=int(input("ideal time minute only >>"))
+if a>=60:
+    print("Given number minutes in 59 Below")
+    print("Try Again ")
+
+time_stop=time_h*60+time_m+a
+print("Quit timing press Ctrl+c")
+print(">>>>>>>>>>>>>>")
+print(">>>>>>>>>>>>>>")
+
+try:
+    while(1):
+        time_c=t.datetime.now()
+        timem_c=time_c.minute
+        timeh_c=time_c.hour
+        time_current=timeh_c*60+timem_c
+        time_show=str(time_c.hour)+':'+str(time_c.minute)+':'+str(time_c.second)
+        print(time_show+'\r',end=" ")
+
+        try:
+            while(time_current==time_stop):
+                print("times up pls check ideal time")
+                winsound.PlaySound('Welcome1.wav', winsound.SND_FILENAME)
+        except KeyboardInterrupt:
+            print("stop")
+            exit()
+
+            
+except KeyboardInterrupt: #quit preess
+    print("stop timing")
+    exit()
